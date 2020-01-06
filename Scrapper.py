636f9a3a7d00751afe4c47a9088e272fa167551b
kwonhyeokmin <hykwon8952@gmail.com>
2020-01-06 17:24:59 +0900

    initial commit

--- Scrapper.py
+++ Scrapper.py
@@ -10,9 +10,10 @@ import config
 import pandas as pd
 from datetime import datetime
 
+
 def configure_logging():
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
     handler.setFormatter(
         logging.Formatter('[%(asctime)s %(levelname)s %(module)s]: %(message)s'))
@@ -20,55 +21,68 @@ def configure_logging():
     return logger
 
 
-# Set logger
-logger = configure_logging()
-# Set directory
-cfg = config
-TARGET_PATH = osp.join(cfg.DATA_DIR, 'target', 'data_for_scraping.csv')
-IMG_DIR = osp.join(cfg.DATA_DIR, 'image')
+def run():
+    try:
+        target = pd.read_csv(TARGET_PATH, encoding='latin1')[['English Name', 'Link']]
+    except IOError as e:
+        logger.error('Please locate data_for_scapping.csv file in target directory')
+        raise IOError('Please locate data_for_scapping.csv file in target directory')
+    header = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+    }
+    result = {'person_nm': [], 'image_path': [], 'link': []}
+    for i in target.index:
+        person_nm, page_url = tuple(target.iloc[i])
+        person_nm = person_nm.replace(' ', '_')
 
-target = pd.read_csv(TARGET_PATH, encoding='latin1')[['English Name', 'Link']]
-header = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
-}
-result = {'person_nm': [], 'image_path': [], 'link': []}
-for i in target.index:
-    person_nm, page_url = tuple(target.iloc[i])
-    person_nm = person_nm.replace(' ', '_')
+        soup = BeautifulSoup(urlopen(Request(url=page_url, headers=header)), 'html.parser')
 
-    soup = BeautifulSoup(urlopen(Request(url=page_url, headers=header)), 'html.parser')
+        images = []  # contains the link for Large original images, type of  image
+        for a in soup.find_all("div", {"class": "rg_meta"}):
+            link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
+            images.append((link, Type))
+        for j, (url, image_type) in enumerate(tqdm(images[:min(len(images), cfg.max_count)], desc=person_nm)):
+            try:
+                req = Request(url, headers=header)
+                resp = urlopen(req)
+                raw_image = resp.read()
+                extension = image_type if image_type else 'jpg'
+                if extension.lower() not in cfg.extensions:
+                    continue
+                file_name = uuid.uuid4().hex
+                save_path = os.path.join(IMG_DIR, person_nm)
+                if not osp.isdir(save_path):
+                    os.mkdir(save_path)
+                # Save image file
+                with open(osp.join(save_path, '{}.{}'.format(file_name, extension)), 'wb') as image_file:
+                    image_file.write(raw_image)
 
-    images = []  # contains the link for Large original images, type of  image
-    logger.info('start make url list')
-    for a in soup.find_all("div", {"class": "rg_meta"}):
-        link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
-        images.append((link, Type))
-    for j, (url, image_type) in enumerate(tqdm(images[:min(len(images), cfg.max_count)], desc=person_nm)):
-        try:
-            req = Request(url, headers=header)
-            resp = urlopen(req)
-            raw_image = resp.read()
-            extension = image_type if image_type else 'jpg'
-            if extension.lower() not in cfg.extensions:
+                result['person_nm'].append(person_nm)
+                result['image_path'].append(osp.join('image', cfg.site_name, person_nm, '{}.{}'.format(file_name, extension)))
+                result['link'].append(url)
+
+            except Exception as e:
+                logger.debug(str(url))
                 continue
-            file_name = uuid.uuid4().hex
-            save_path = os.path.join(IMG_DIR, person_nm)
-            if not osp.isdir(save_path):
-                os.mkdir(save_path)
 
-            with open(osp.join(save_path, '{}.{}'.format(file_name, extension)), 'wb') as image_file:
-                image_file.write(raw_image)
+    # Save result
+    df = pd.DataFrame.from_dict(result)
+    now = datetime.now()
+    date_time = now.strftime("%Y%m%d_%H:%M:%S")
+    df.to_excel(osp.join(cfg.DATA_DIR, "{}.xlsx".format(date_time)), index=False)
 
-            result['person_nm'].append(person_nm)
-            result['image_path'].append(osp.join('image', cfg.site_name, person_nm, file_name))
-            result['link'].append(url)
 
-        except Exception as e:
-            logger.error(str(url))
-            continue
+if __name__ == '__main__':
+    # Set logger
+    logger = configure_logging()
+    cfg = config
 
-# Save result
-df = pd.DataFrame.from_dict(result)
-now = datetime.now()
-date_time = now.strftime("%Y%m%d_%H:%M:%S")
-df.to_excel(osp.join(cfg.DATA_DIR, "{}.xlsx".format(date_time)), index=False)
+    # Set directory
+    if not osp.isdir(cfg.TARGET_DIR):
+        os.mkdir(cfg.TARGET_DIR)
+    if not osp.isdir(cfg.IMG_DIR):
+        os.mkdir(cfg.IMG_DIR)
+    TARGET_PATH = osp.join(cfg.TARGET_DIR, 'data_for_scraping.csv')
+    IMG_DIR = cfg.IMG_DIR
+    # Application start
+    run()
--- config.py
+++ config.py
@@ -6,7 +6,9 @@ ROOT_DIR = os.getcwd()
 DRIVER_PATH = osp.join(ROOT_DIR, 'driver', 'chromedriver')
 # Data path
 DATA_DIR = osp.join(ROOT_DIR, 'data')
+TARGET_DIR = osp.join(DATA_DIR, 'target')
+IMG_DIR = osp.join(DATA_DIR, 'image')
 # src
-max_count = 10
+max_count = 100
 extensions = ['jpg', 'jpeg', 'png']
 site_name = 'google'
